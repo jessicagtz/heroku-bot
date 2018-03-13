5e3e3a3260ce2a786013d83190f02e60a8c115f7
Jessica Gutierrez <jessicagutierrez@dhcp-10-102-215-139.wireless.northwestern.private>
2018-03-12 19:26:36 -0500

    i commit

--- ChatterBot.py
+++ ChatterBot.py
@@ -17,3 +17,26 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a function that tweets
 # CODE GOES HERE
+# Create a function that tweets
+def TweetOut(tweet_number):
+    api.update_status(
+        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        tweet_number)
+
+
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+# Infinitely loop
+t_end = time.time() + 60 * 5
+while(time.time() < t_end):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(counter)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    counter = counter + 1
+    
